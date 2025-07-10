1c93f4b6c52a47e92a017ba7c9fb42ef5771681f
LouisePrd <louise.peridy22@gmail.com>
2025-07-10 21:08:15 +0200

    🚧 Setup queries

--- src/app/database/queries/users.ts
+++ src/app/database/queries/users.ts
@@ -1,11 +1,22 @@
-import { users } from "../schema";
 import { eq } from "drizzle-orm";
-import { db } from "../../../db";
+import { db } from "@/db";
+import { users } from "../schema";
+import bcrypt from "bcrypt";
 
-export async function getAllUsers() {
-  return await db.select().from(users);
+export async function createUser(pseudo: string, password: string) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const result = await db.insert(users).values({ pseudo, password: hashedPassword }).returning();
+  return result[0];
 }
 
-export async function getUserById(id: number) {
-  return await db.select().from(users).where(eq(users.id_user, id));
+export async function connectUser(pseudo: string, password: string) {
+  const result = await db.select().from(users).where(eq(users.pseudo, pseudo));
+  const user = result[0];
+
+  if (!user) return null;
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect) return null;
+
+  return user;
 }
