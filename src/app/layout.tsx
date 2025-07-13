ffb458783ff5e0ba8dd022cc0272bf26ea5512cc
LouisePrd <louise.peridy22@gmail.com>
2025-07-13 12:21:53 +0200

    🚚 Rename import and component files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navbar/navbar";
-import Footer from "./components/footer/footer";
+import Navbar from "./components/Navbar/navbar";
+import Footer from "./components/Footer/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
